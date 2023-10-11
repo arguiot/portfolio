f8c415d1da56c8ea6df81a9c40b40fa12a3187ec
Arthur Guiot <arguiot@gmail.com>
2023-10-11 16:30:22 -0700

    Fixed risk parity

--- deploy/main.tf
+++ deploy/main.tf
@@ -21,7 +21,45 @@ resource "azurerm_public_ip" "publicip" {
   name                = "${var.prefix}-publicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "${var.prefix}-dev" /* This is a unique label for dns, e.g. xyz.cloudapp.net */
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  // Allow HTTP traffic
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -67,19 +105,32 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   disable_password_authentication = true
 
-  #   provisioner "remote-exec" {
-  #     inline = [
-  #       "sudo apt-get update",
-  #       "sudo apt-get install -y python3-pip",
-  #       "pip3 install notebook",
-  #       "nohup jupyter notebook --ip=0.0.0.0 --no-browser &"
-  #     ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo docker run --detach -p 80:8888 -v /home/adminuser:/home/jovyan/work --name jupyter jupyter/scipy-notebook start-notebook.sh --PasswordIdentityProvider.hashed_password='argon2:$argon2id$v=19$m=10240,t=10,p=8$gytfRHsuWwpFOtGxMJcPbw$wHvu6Nj+Mp++LxIFOlBxq6Am/lrmfUS+z5F136GbDz8'",
+      "type -p gh >/dev/null || (sudo apt update && sudo apt install gh -y)",
+      "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg",
+      "sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null",
+      "sudo apt update",
+      "sudo apt install gh -y",
+      "gh auth login --with-token ${file(".github_token")}",
+      "gh auth setup-git",
+      "git clone --depth 1 --recurse-submodules https://github.com/EveryFinance/backtesting-and-trading"
+    ]
 
-  #     connection {
-  #       type        = "ssh"
-  #       user        = "adminuser"
-  #       private_key = file("~/.ssh/id_rsa") # Adjust as per requirements.
-  #       host        = self.public_ip_address
-  #     }
-  #   }
+    connection {
+      type = "ssh"
+      user = "adminuser"
+      host = self.public_ip_address
+    }
+  }
 }
--- portfolio_optimization/optimization/risk_parity.py
+++ portfolio_optimization/optimization/risk_parity.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.typing import NDArray
 import pandas as pd
 from pypfopt.risk_models import CovarianceShrinkage, sample_cov
-import riskparityportfolio as rp
+from .risk_parity_impl import *
 
 
 class RiskParity(GeneralOptimization):
@@ -40,7 +40,7 @@ class RiskParity(GeneralOptimization):
         budget = np.ones(self.cov_matrix.shape[0]) / self.cov_matrix.shape[0]
 
         # Optimize the portfolio
-        pf = rp.RiskParityPortfolio(
+        pf = RiskParityPortfolio(
             covariance=self.cov_matrix,
             budget=budget,
         )
@@ -75,70 +75,3 @@ class RiskParity(GeneralOptimization):
             return sample_cov(self.df)
         elif self.mode == self.Mode.LEDOIT_WOLF:
             return CovarianceShrinkage(self.df).ledoit_wolf()
-
-
-# class RiskParity(GeneralOptimization):
-#     def __init__(self, df, mcaps=None, weight_bounds=(0, 1)):
-#         self.weight_bounds = weight_bounds
-#         super().__init__(df, mcaps)
-
-#     # risk budgeting optimization
-#     def calculate_portfolio_var(self, w, V):
-#         # function that calculates portfolio risk
-#         w = np.matrix(w)
-#         return (w * V * w.T)[0, 0]
-
-#     def calculate_risk_contribution(self, w, V):
-#         # function that calculates asset contribution to total risk
-#         w = np.matrix(w)
-#         sigma = np.sqrt(self.calculate_portfolio_var(w, V))
-#         # Marginal Risk Contribution
-#         MRC = V * w.T
-#         # Risk Contribution
-#         RC = np.multiply(MRC, w.T) / sigma
-#         return RC
-
-#     def risk_budget_objective(self, x, pars):
-#         # calculate portfolio risk
-#         V = pars[0]  # covariance table
-#         x_t = pars[1]  # risk target in percent of portfolio risk
-#         sig_p = np.sqrt(self.calculate_portfolio_var(x, V))  # portfolio sigma
-#         risk_target = np.asmatrix(np.multiply(sig_p, x_t))
-#         asset_RC = self.calculate_risk_contribution(x, V)
-#         J = sum(np.square(asset_RC - risk_target.T))[0, 0]  # sum of squared error
-#         return J
-
-#     def total_weight_constraint(self, x):
-#         return np.sum(x) - 1.0
-
-#     def long_only_constraint(self, x):
-#         return self.weight_bounds[1] - x
-
-#     def short_only_constraint(self, x):
-#         return x - self.weight_bounds[0]  # Ensuring weights are not less than
-
-#     def get_weights(self):
-#         # 1 / N risk portfolio
-#         x_t = [1 / self.df.shape[1]] * self.df.shape[1]
-#         cons = (
-#             {"type": "eq", "fun": self.total_weight_constraint},
-#             {"type": "ineq", "fun": self.short_only_constraint},
-#             {"type": "ineq", "fun": self.long_only_constraint},
-#         )
-
-#         w0 = np.ones(self.df.shape[1]) * (1.0 / self.df.shape[1],)
-#         V = np.cov(self.df.T)
-
-#         res = minimize(
-#             self.risk_budget_objective,
-#             w0,
-#             args=[V, x_t],
-#             method="SLSQP",
-#             constraints=cons,
-#             options={"disp": True, "ftol": 1e-9},
-#         )
-#         weights = pd.Series(res.x, index=self.df.columns)
-#         return weights
-
-#     def get_metrics(self):
-#         pass
