a750cd0727727b7007f4ff4a86e1a09a3f198da5
Arthur Guiot <arguiot@gmail.com>
2024-09-25 16:33:00 -0700

    Updated parity

--- portfolio_optimization/backtesting/parity.py
+++ portfolio_optimization/backtesting/parity.py
@@ -12,13 +12,12 @@ from enum import Enum
 
 class ParityLine:
     def __init__(self):
-        self.a = 0.0
-        self.b = 0.0
         self.weight_A = 0.8
         self.weight_B = 0.2
         self.sigma_a = 0.0
         self.sigma_b = 0.0
         self.sigma_c = 0.0
+        self.sigma_ab = 0.0
         self.r_a = 0.0
         self.r_b = 0.0
         self.r_c = 0.0
@@ -72,120 +71,64 @@ class ParityLine:
             "Portfolio Value"
         ].pct_change().std() * np.sqrt(365)
 
-        r_ab = self.weight_A * self.r_a + self.weight_B * self.r_b
-        risk_ab = self.weight_A * self.sigma_a + self.weight_B * self.sigma_b
-
-        # Linear regression on AB and G
-        start_date_c = portfolio_g.portfolio_value["Portfolio Value"].index[0]
-        end_date_c = portfolio_g.portfolio_value["Portfolio Value"].index[-1]
-        years_c = (end_date_c - start_date_c).days / 365
-        self.r_c = (
-            (
-                portfolio_g.portfolio_value["Portfolio Value"].iloc[-1]
-                / portfolio_g.portfolio_value["Portfolio Value"].iloc[0]
-            )
-            ** (1 / years_c)
-        ) - 1
+        # Add the two portfolio values (weights * value) to compute sigma_ab
+        portfolio_ab = (
+            portfolio_a.portfolio_value["Portfolio Value"] * self.weight_A
+            + portfolio_b.portfolio_value["Portfolio Value"] * self.weight_B
+        )
+        self.sigma_ab = portfolio_ab.pct_change().std() * np.sqrt(365)
         self.sigma_c = portfolio_g.portfolio_value[
             "Portfolio Value"
         ].pct_change().std() * np.sqrt(365)
 
-        # Risk is x, return is y, we need to find a and b such that y = ax + b
-        a = (self.r_c - r_ab) / (self.sigma_c - risk_ab)
-        b = self.r_c - a * self.sigma_c
-        self.a, self.b = a, b
-
-    def getMinReturn(self):
-        return self.r_c
-
-    def getMaxReturn(self):
-        return max(self.r_a, self.r_b)
-
     def getMinRisk(self):
-        risk, _, _, _ = self.convertReturn(self.getMinReturn())
-        return risk
+        return 0
 
     def getMaxRisk(self):
-        risk, _, _, _ = self.convertReturn(self.getMaxReturn())
-        return risk
-
-    def setSigma(self, sigma_a, sigma_b, sigma_c):
-        """
-        Sets the risk of each portfolio (Alpha, Beta, Gamma)
-        """
-        self.sigma_a, self.sigma_b, self.sigma_c = sigma_a, sigma_b, sigma_c
-
-    def setParityLineCoeff(self, a, b):
-        self.a, self.b = a, b
-
-    def setReturns(self, r_a, r_b, r_c):
-        self.r_a, self.r_b, self.r_c = r_a, r_b, r_c
-
-    def setWeightCoeff(self, weight_A, weight_B):
-        self.weight_A, self.weight_B = weight_A, weight_B
-
-    def convertRisk(self, risk):
-        if not (
-            (risk >= self.getMinRisk())
-            and (risk <= self.getMaxRisk())
-            or (
-                np.isclose(risk, self.getMinRisk(), rtol=1e-5)
-                or np.isclose(risk, self.getMaxRisk(), rtol=1e-5)
-            )
-        ):
-            if risk < self.getMinRisk():
-                risk = self.getMinRisk()
-            elif risk > self.getMaxRisk():
-                risk = self.getMaxRisk()
-        _return = (self.a * risk) + self.b
-        weights = self._calculateWeights(_return)
-        return _return, weights
+        return 0.8
 
     def convertReturn(self, _return):
-        assert (
-            (_return >= self.getMinReturn())
-            and (_return <= self.getMaxReturn())
-            or (
-                np.isclose(_return, self.getMinReturn(), rtol=1e-5)
-                or np.isclose(_return, self.getMaxReturn(), rtol=1e-5)
-            )
-        ), f"Return {_return} is not between {self.getMinReturn()} and {self.getMaxReturn()}"
-        risk = (_return - self.b) / self.a
-        weights = self._calculateWeights(_return)
-        return risk, weights[0], weights[1], weights[2]
+        return_riskless = 0.1
+        return_risky = 0.5
+        sigma_riskless = self.sigma_c
+        sigma_risky = self.sigma_ab
+
+        target_sigma = min(
+            max(
+                sigma_riskless
+                + ((_return - return_riskless) / (return_risky - return_riskless))
+                * (sigma_risky - sigma_riskless),
+                sigma_riskless,
+            ),
+            0.8,  # 80%
+        )
+
+        return target_sigma
 
     def convertWeights(self, weight_alpha, weight_beta, weight_gamma):
         _return = (
             weight_alpha * self.r_a + weight_beta * self.r_b + weight_gamma * self.r_c
         )
-        risk = (_return - self.b) / self.a
-        return risk, _return
+        return _return
 
-    def _calculateWeights(self, _return):
-        _combinedReturn = float(self.weight_A * self.r_a + self.weight_B * self.r_b)
-        _w_Beta_Ind = 1 if _return <= _combinedReturn else 0
-        _g_weight = self.g_weight(_return, _combinedReturn, _w_Beta_Ind)
-        _c_weight = self.c_weight(_return, _combinedReturn, _w_Beta_Ind)
-        _g_trim = np.clip(_g_weight, 0, 1)
-        _c_trim = np.clip(_c_weight, 0, 1)
-        weight_beta = _c_trim * self.weight_B * _w_Beta_Ind
-        weight_gamma = _g_trim
-        weight_alpha = 1 - weight_beta - weight_gamma
-        return weight_alpha, weight_beta, weight_gamma
-
-    def g_weight(self, _return, _combinedReturn, _w_Beta_Ind):
-        if _w_Beta_Ind == 1:
-            if _combinedReturn >= _return:
-                return (_combinedReturn - _return) / (_combinedReturn - self.r_c)
+    def _calculateWeights(self, _risk):
+        floor = self.getMinRisk()
+        cap = self.getMaxRisk()
+        if self.sigma_ab > self.sigma_c:
+            w = min(
+                max((_risk - self.sigma_c) / (self.sigma_ab - self.sigma_c), floor), cap
+            )
         else:
-            if self.r_a >= _return:
-                return (self.r_a - _return) / (self.r_a - self.r_c)
-        return 0
+            w = cap
 
-    def c_weight(self, _return, _combinedReturn, _w_Beta_Ind):
-        if _w_Beta_Ind != 0 and _return >= self.r_c:
-            return (_return - self.r_c) / (_combinedReturn - self.r_c)
-        return 0
+        weight_risky = w
+        weight_riskless = 1 - w
+
+        weight_a = weight_risky * self.weight_A
+        weight_b = weight_risky * self.weight_B
+        weight_c = weight_riskless
+
+        return weight_a, weight_b, weight_c
 
 
 class ParityProcessorDelegate:
@@ -203,7 +146,8 @@ class ParityProcessorDelegate:
             self.risk = 0.60
 
     def compute_weights(self, parity_line: ParityLine) -> pd.Series:
-        _return, weights = parity_line.convertRisk(self.risk)
+        weights = parity_line._calculateWeights(self.risk)
+        _return = parity_line.convertWeights(weights[0], weights[1], weights[2])
         print(f"Weights: {weights}")
         print(f"Return: {_return}")
         # List all the attributes of the parity line
